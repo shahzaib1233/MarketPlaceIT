03bd4231755e74b5a779ef14d216ca819eefd6b9
shahzaib1233 <shahzaibimtiazbt@gmail.com>
2025-02-07 12:33:42 +0500

    test

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'ssms9fkz',       // Apna Sanity Project ID yahan dalen
+  projectId: 'vw03c0oq',       // Apna Sanity Project ID yahan dalen
   dataset: 'production',       // Apna Sanity dataset yahan dalen
   apiVersion: '2025-01-21',    // API version specify karein
   useCdn: process.env.NODE_ENV === 'production', // Use CDN in production
